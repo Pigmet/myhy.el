732696d478f417d35b96001d8f80a51a410fe241
Gou <nakamura462645@gmail.com>
2020-05-23 22:34:14 +0900

    added defun myhy-eval-last-sexp-buffer ()

--- myhy.el
+++ myhy.el
@@ -1,9 +1,26 @@
 ;;Elisp program for hy source code editing. 
+(require 'eros)
+(require 'hy-shell)
+
+(defun myhy--eval-last-sexp-string ()
+  ( hy-shell--redirect-send   (hy--last-sexp-string)))
 
 (defun myhy-eval-last-sexp ()
   (interactive)
-  (mylet [res (myhy--send-text (hy--last-sexp-string))]
+  (mylet [res (myhy--eval-last-sexp-string)]
 	 (eros--eval-overlay res (point))))
+
+(setq myhy-result (generate-new-buffer "myhy"))
+
+(defun myhy-eval-last-sexp-buffer ()
+  (interactive)
+  (mylet [res (myhy--eval-last-sexp-string)]
+	 (with-current-buffer myhy-result
+	   (erase-buffer)
+	   (python-mode)
+	   (insert res)
+	   (beginning-of-buffer))
+	 (switch-to-buffer-other-window myhy-result)))
 
 (provide 'myhy)
 
