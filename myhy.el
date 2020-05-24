b6d0ec48c222798d800b1366f37a3d85f1113f0e
Gou <nakamura462645@gmail.com>
2020-05-24 11:21:47 +0900

    added myhy-doc

--- myhy.el
+++ myhy.el
@@ -2,15 +2,17 @@
 (require 'eros)
 (require 'hy-shell)
 
+;; TODO provide a document lookup utility
+
 (defun myhy--eval-last-sexp-string ()
-  ( hy-shell--redirect-send   (hy--last-sexp-string)))
+  (hy-shell--redirect-send (hy--last-sexp-string)))
 
 (defun myhy-eval-last-sexp ()
   (interactive)
   (mylet [res (myhy--eval-last-sexp-string)]
 	 (eros--eval-overlay res (point))))
 
-(setq myhy-result (generate-new-buffer "myhy"))
+(setq myhy-result (generate-new-buffer "*myhy-result*"))
 
 (defun myhy-eval-last-sexp-buffer ()
   (interactive)
@@ -22,6 +24,31 @@
 	   (beginning-of-buffer))
 	 (switch-to-buffer-other-window myhy-result)))
 
+;; doc
+
+(setq myhy-doc (generate-new-buffer "*myhy-doc*"))
+
+(defun myhy--doc-as-string (text)
+  (hy-shell--redirect-send
+   (format "(print %s.__doc__)" text )))
+
+(defun myhy--last-word ()
+  (save-excursion
+    (re-search-backward (rx (+ word)) (line-beginning-position) t)
+    (message "%s"  (thing-at-point 'word))))
+
+(defun myhy-doc ()
+  (interactive)
+  (mylet [word (myhy--last-word)
+	       res (read-string "doc for:" word)
+	       s (->>(myhy--doc-as-string res)
+		     (s-split "\n")
+		     (-drop 1)
+		     (s-join "\n"))]
+	 (with-current-buffer myhy-doc
+	   (erase-buffer)
+	   (insert s)
+	   (beginning-of-buffer))
+	 (switch-to-buffer-other-window myhy-doc)))
+
 (provide 'myhy)
-
-
